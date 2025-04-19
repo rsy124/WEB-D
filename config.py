c55d2639138577f4e23a9d5e1dacb0008112962d
Rutuja Yadav <rutujayadav33.10mes@gmail.com>
2025-04-19 12:09:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# Store API keys securely
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # OpenAI API Key
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")  # Pinecone API Key
+SERPAPI_KEY ="852455e01a3f64afa"
+NEWS_API_KEY="f67a5fc7937547a0ae5aedc72ebd89c9"
+#WIKIPEDIA_API_KEY = ""
